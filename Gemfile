a62726dbc396a5417770e2aec72b3c69ee9aa238
Don Park <donp@donp.org>
2015-01-30 10:04:45 -0800

    index page

--- Gemfile
+++ Gemfile
@@ -2,6 +2,5 @@ source 'https://rubygems.org'
 
 gem 'rack'
 gem 'slim'
-gem 'moneta'
 gem 'lmdb'
 gem 'puma'
--- rackblog.rb
+++ rackblog.rb
@@ -1,6 +1,6 @@
 require 'json'
 require 'slim'
-require 'moneta'
+require 'lmdb'
 
 class Rackblog
   def initialize
@@ -8,7 +8,9 @@ class Rackblog
     @layout = Slim::Template.new('views/layout.slim')
     @article = Slim::Template.new('views/article.slim')
     @post = Slim::Template.new('views/post.slim')
-    @db = Moneta.new(:LMDB, {dir: 'db'})
+    @index = Slim::Template.new('views/index.slim')
+    lmdb = LMDB.new('db')
+    @db = lmdb.database
   end
 
   def call(env)
@@ -17,21 +19,24 @@ class Rackblog
     puts "** db load #{env['REQUEST_PATH'].inspect} decode: #{path}"
     headers = {'Content-Type' => 'text/html'}
 
-    key = path.split('/')[1]
-    puts "Key #{key.inspect}"
-    if key == 'post'
+    if path == '/'
+      html = index
+    elsif path.split('/')[1] == 'post'
       if env['REQUEST_METHOD'] == 'GET'
         html = @post.render
       elsif env['REQUEST_METHOD'] == 'POST'
         slug = article_save(req.params)
         puts "Slug: #{slug}"
-        post_url = "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}/#{slug}"
+        post_url = "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{slug}"
         puts "Redirect: #{post_url}"
         return [302, headers.merge({"Location" => post_url}), []]
       end
-    elsif @db.key?(key)
-      params = JSON.parse(@db[key])
-      html = article(params)
+    else
+      json = @db.get(path)
+      if json
+        params = JSON.parse(json)
+        html = layout(@article, params)
+      end
     end
 
     if html
@@ -41,9 +46,30 @@ class Rackblog
     end
   end
 
-  def article(params)
+  def index
+    articles = []
+    puts @db.stat.inspect
+    if @db.stat[:entries] > 0
+      @db.cursor do |cursor|
+        articles << cursor.last if articles.empty?
+        15.times do
+          next_art = cursor.prev
+          if next_art
+            next_art[1] = JSON.parse(next_art[1])
+            articles << next_art
+          else
+            break
+          end
+        end
+      end
+    end
+    puts articles.inspect
+    layout(@index, {articles: articles})
+  end
+
+  def layout(template, params)
     @layout.render do |layout|
-      @article.render(nil, params)
+      template.render(nil, params)
     end
   end
 
@@ -53,7 +79,9 @@ class Rackblog
 
   def article_save(data)
     puts data.inspect
-    slug = to_slug(data['title'])
+    now = Time.now
+    data['time'] = now.iso8601
+    slug = to_slug("/#{now.year}/#{"%02d"%now.month}/#{"%02d"%now.day}/#{data['title']}")
     puts "Saving Key #{slug.inspect}"
     @db[slug] = data.to_json
     URI.encode(slug)
