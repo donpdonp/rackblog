4d2ec1528dff3bb0ce884014fd2424ce9e7ff23c
Don Park <donp@donp.org>
2015-01-29 18:03:02 -0800

    layout.slim

--- rackblog.rb
+++ rackblog.rb
@@ -1,14 +1,16 @@
-require 'slim'
+require 'slim/include'
 
 class Rackblog
   def initialize
     Slim::Engine.set_options({pretty: true})
+    @layout = Slim::Template.new('views/layout.slim')
   end
 
   def call(env)
     template = template_for(env['REQUEST_PATH'])
     if template
-      ['200', {'Content-Type' => 'text/html'}, [template.render]]
+      html = render(template)
+      ['200', {'Content-Type' => 'text/html'}, [html]]
     else
       ['404', {'Content-Type' => 'text/html'}, ['Page not found']]
     end
@@ -31,4 +33,14 @@ class Rackblog
     end
   end
 
+  def render(template)
+    puts "about to render"
+    @layout.render do |layout|
+      puts "i am layout block"
+      template.render
+    end
+  end
+end
+
+module Helper
 end
