a8717785ed895b8305c9124e4457e0277b42b334
Don Park <donp@donp.org>
2015-02-09 16:37:59 -0800

    d3 in the house

---
+++ public/blog/js/app.js
@@ -0,0 +1,8 @@
+function d3go() {
+  var circles = d3.selectAll("#three circle")
+  circles.transition()
+        .delay(200)
+        .duration(7100)
+        .ease('elastic')
+        .attr("cx", function(who) {return Math.random() * 120; });
+}
