ca24b8cd5640cddeff074cc6500b5545ebd3cd9d
Don Park <donp@donp.org>
2018-11-02 14:46:38 -0700

    favicon

--- public/blog/js/app.js
+++ public/blog/js/app.js
@@ -1,11 +1,16 @@
 function d3go() {
   var circles = d3.selectAll("#three circle")
-  circles.transition()
-        .delay(200)
-        .duration(7100)
-        .ease('elastic')
-        .attr("cx", function(who) {return 10+(Math.random() * 190); });
+  jiggle(circles)
   if (typeof d3ready !== 'undefined') {
     d3ready()
   }
+}
+
+function jiggle(things) {
+  things.transition()
+        .delay(200)
+        .duration(7100)
+        .ease('elastic')
+        .attr("cx", function(who) {return 10+(Math.random() * 190); })
+        .each("end", function(){jiggle(things)})
 }
